f9c634158582160dbdb0e221ce9fe91ffeeb53ab
gitlisso <lisso@nastyle.pro>
2025-01-24 02:32:39 +0300

    upf * app.head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,13 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
+  app: {
+    head: {
+      title: 'NuxtVerse - совеременный и быстрый блог на Nuxt.js',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
