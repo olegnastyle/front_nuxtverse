c70d441e26e42c38bbb7f1ade0a0b4837b9a0a8d
gitlisso <lisso@nastyle.pro>
2025-01-24 02:02:27 +0300

    fix * devServer => host

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  devServer: {
+    host: '0.0.0.0',
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
