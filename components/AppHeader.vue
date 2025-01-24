1b27a936a1110b81e4bb57f36786b58d8dc17730
gitlisso <lisso@nastyle.pro>
2025-01-24 03:34:07 +0300

    upd * header > accordion

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -50,7 +50,7 @@ import { NuxtLink } from '#components';
                         </li>
                         <li>
                             <input type="checkbox" id="accordion" class="hidden peer/accordion">
-                            <label for="accordion" class="flex justify-between py-3 text-black cursor-pointer peer-checked/accordion:*:rotate-180" href="#">Услуги
+                            <label for="accordion" class="flex justify-between py-3 text-black peer-checked/accordion:text-teal-600 cursor-pointer peer-checked/accordion:*:rotate-180" href="#">Услуги
                                 <span class="text-black/50">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
